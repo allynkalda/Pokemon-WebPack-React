86d2902bd8e0864bd4255546e80c29ff7aa00cfe
allyn <allyn.alda@gmail.com>
2020-11-01 11:32:24 +0100

    add keydown in search

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -7,9 +7,14 @@ export default function SearchInput({ handleClick }) {
     const handleChange = (e) => {
         setValue(e.target.value);
     };
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleClick(value);
+        }
+    };
     return (
         <div className="inputBox">
-            <input type="text" className="input" placeholder="Find a Pokemon" onChange={(e) => handleChange(e)}></input>
+            <input type="text" className="input" placeholder="Find a Pokemon" onChange={(e) => handleChange(e)} onKeyDown={handleKeyDown}></input>
             <button className="search-button" type="button" aria-label="search" onClick={() => handleClick(value)}>
                 <img className="loupe" src="../image/loupe.png" />
             </button>
